3887a49878bfb444f97d5f7289bd70e82fb900c9
11t518s <11t518s20161759@gmail.com>
2026-01-20 17:09:39 +0900

    slack bot 에러를 수정합니다

--- packages/obligation-bot/src/slack/socket-app.ts
+++ packages/obligation-bot/src/slack/socket-app.ts
@@ -3,35 +3,12 @@ import { publishAppHome } from "./publish";
 import { parseSlackActionId } from "./actions";
 import { createSlackSocketAppContext } from "../di";
 import { startApiServer } from "../api/server";
-import { WorkflowVisualizationService, workflowVizEvents } from "../workflow-viz";
 import { normalizeSlackEvent } from "../normalize/slack";
 
 export async function startSlackSocketApp(): Promise<void> {
   const { service, signingSecret, botToken, appToken } = createSlackSocketAppContext();
 
   void startApiServer(service);
-
-  const workflowViz = new WorkflowVisualizationService({ botToken });
-
-  workflowVizEvents.on("session:start", (session) => {
-    workflowViz.startSession(session).catch((err) => {
-      console.error("[workflow-viz] Error starting session:", err);
-    });
-  });
-
-  workflowVizEvents.on("session:end", ({ sessionId }) => {
-    workflowViz.endSession(sessionId).catch((err) => {
-      console.error("[workflow-viz] Error ending session:", err);
-    });
-  });
-
-  workflowVizEvents.on("workflow:event", (event) => {
-    workflowViz.handleEvent(event).catch((err) => {
-      console.error("[workflow-viz] Error handling event:", err);
-    });
-  });
-
-  console.log("[workflow-viz] Service initialized and EventEmitter connected");
 
   const app = new App({
     token: botToken,
